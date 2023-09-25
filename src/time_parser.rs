8bfd08edf8939ee4c5ea15cbbe86d77f941cb4f6
dante1130 <andrewdante2@gmail.com>
2023-09-25 13:20:30 +0800

    implement string formatting for Duration

--- src/time_parser.rs
+++ src/time_parser.rs
@@ -3,7 +3,7 @@ use chrono::Duration;
 pub enum TimeParser {}
 
 pub enum TimeParserError {
-    InvalidNumberFormat,    
+    InvalidNumberFormat,
     InvalidTimeFormat,
 }
 
@@ -39,5 +39,42 @@ impl TimeParser {
         }
 
         Ok(duration)
+    }
+
+    pub fn duration_to_string(duration: &Duration) -> String {
+        let mut duration_string = String::new();
+
+        const DAYS_IN_WEEK: i64 = 7;
+        const HOURS_IN_DAY: i64 = 24;
+        const MINUTES_IN_HOUR: i64 = 60;
+
+        let weeks = duration.num_weeks();
+        let days = duration.num_days() - weeks * DAYS_IN_WEEK;
+
+        let hours =
+            duration.num_hours() - weeks * DAYS_IN_WEEK * HOURS_IN_DAY - days * HOURS_IN_DAY;
+
+        let minutes = duration.num_minutes()
+            - weeks * DAYS_IN_WEEK * HOURS_IN_DAY * MINUTES_IN_HOUR
+            - days * HOURS_IN_DAY * MINUTES_IN_HOUR
+            - hours * MINUTES_IN_HOUR;
+
+        if weeks > 0 {
+            duration_string.push_str(&format!("{}w ", weeks));
+        }
+
+        if days > 0 {
+            duration_string.push_str(&format!("{}d ", days));
+        }
+
+        if hours > 0 {
+            duration_string.push_str(&format!("{}h ", hours));
+        }
+
+        if minutes > 0 {
+            duration_string.push_str(&format!("{}m", minutes));
+        }
+
+        duration_string.trim().into()
     }
 }
